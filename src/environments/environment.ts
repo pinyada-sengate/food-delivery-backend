d4979a158d96e9811612a9360533e3db9ffdec9e
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-04-15 17:10:45 +0200

    git add env

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+import { DevEnvironment } from "./environment.dev";
+import { ProdEnvironment } from "./environment.prod";
+
+export interface Environment {
+  mongoURI: string;
+}
+
+export function getEnviromentVariables() {
+  if (process.env.NODE_ENV === "production") {
+    return ProdEnvironment;
+  }
+
+  return DevEnvironment;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import * as express from "express";
 import * as mongoose from "mongoose";
+import { getEnviromentVariables } from "./environments/environment";
 
 let app: express.Application = express();
-
-const mongoURI: string =
-  "mongodb+srv://pinyadadev:3jMjPrQmwn3THXRy@cluster0.dq3p8ve.mongodb.net/fooddeliverydb?retryWrites=true&w=majority";
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-mongoose.connect(mongoURI).then(() => {
+mongoose.connect(getEnviromentVariables().mongoURI).then(() => {
   console.log("Connected to mongodb");
 });
