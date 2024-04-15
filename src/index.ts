01ed764e80e54b6027dbe7462058d1f8f6afd8d8
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-04-15 15:56:11 +0200

    connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
 
 let app: express.Application = express();
 
+const mongoURI: string =
+  "mongodb+srv://pinyadadev:3jMjPrQmwn3THXRy@cluster0.dq3p8ve.mongodb.net/fooddeliverydb?retryWrites=true&w=majority";
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+mongoose.connect(mongoURI).then(() => {
+  console.log("Connected to mongodb");
 });
