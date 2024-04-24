7c82864cb0b16a333af64fbf0bca56eb87f52c79
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-04-24 15:30:32 +0200

    modify verification_token to type string

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,7 @@ import { model } from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   email_verified: { type: Boolean, required: true, default: false },
-  verification_token: { type: Number, required: true },
+  verification_token: { type: String, required: true },
   verification_token_time: { type: Date, required: true },
   phone: { type: String, required: true },
   password: { type: String, required: true },
