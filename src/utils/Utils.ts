ace8d77daa7d4dc047f8fba7cc25c11e00d8fcaf
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-04-26 20:57:33 +0200

    add multer

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,6 +1,23 @@
 import * as Bcrypt from "bcrypt";
+import * as Multer from "multer";
 
-import { getEnviromentVariables } from "../environments/environment";
+const storageOptions = Multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./src/uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 export class Utils {
   public static readonly MAX_TOKEN_TIME = 5 * 60 * 1000; // 5 minute
