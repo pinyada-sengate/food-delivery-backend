7298e03abbe346e4275ce5a20d8e3b283c2f4582
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-05-21 09:39:43 +0200

    address model

---
+++ src/models/Address.ts
@@ -0,0 +1,17 @@
+import * as mongoose from "mongoose";
+import { model } from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  user_id: { type: mongoose.Types.ObjectId, required: true },
+  title: { type: String, required: true },
+  address: { type: String, required: true },
+  landmark: { type: String, required: true },
+  house: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lng: { type: Number, required: true },
+  status: { type: Boolean, required: true, default: true }, // true is active, false is inactive
+  created_at: { type: Date, required: true, default: new Date() },
+  updated_at: { type: Date, required: true, default: new Date() },
+});
+
+export default model("addresses", addressSchema);
